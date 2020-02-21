aeb96b7308590d0c65ad6c1e2d8af654c932be75
Dakotah Huey <hueydakotah@gmail.com>
2020-02-20 20:52:04 -0500

    recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,30 @@
 
 import math
 
+# loop over both dictionaries
+# compare values for each between the two
+# find out how many times the left goes into the right for each value
+# get the smallest amount of possible combinations for one ingredient out of all of them
+
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    if set(recipe) == set(ingredients):
+        amounts = []
+        for key in recipe:
+            amounts.append(ingredients[key] // recipe[key])
+            print("amounts", amounts)
+        return min(amounts)
+    else:
+        return 0
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# if __name__ == "__main__":
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = {"milk": 100, "butter": 50, "flour": 5}
+#     ingredients = {"milk": 132, "butter": 48, "flour": 51}
+#     print(
+#         "{batches} batches can be made from the available ingredients: {ingredients}.".format(
+#             batches=recipe_batches(recipe, ingredients), ingredients=ingredients
+#         )
+#     )
